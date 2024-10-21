20f0e7442645935a3aa1f1c090b4b5ab178ae25a
aein <mananniaein1234@gmail.com>
2024-10-21 09:57:13 +0330

    update new Version Formik

--- src/UI/input.tsx
+++ src/UI/input.tsx
@@ -1,18 +1,20 @@
-import { InputHTMLAttributes } from "react";
+import { forwardRef, InputHTMLAttributes } from "react";
 
 type InputProps = {
   lable?: string;
   errorMessage?: string;
 } & InputHTMLAttributes<HTMLInputElement>;
 
-const Input = ({ lable, errorMessage, ...props }: InputProps) => {
-  return (
-    <div className="flex flex-col gap-2 w-full">
-      <label htmlFor="">{lable}</label>
-      <input {...props} />
-      <small className="text-red-500">{errorMessage}</small>
-    </div>
-  );
-};
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ lable, errorMessage, ...props }, ref) => {
+    return (
+      <div className="flex flex-col gap-2 w-full">
+        <label htmlFor="">{lable}</label>
+        <input ref={ref} {...props} />
+        <small className="text-red-500">{errorMessage}</small>
+      </div>
+    );
+  }
+);
 
 export default Input;
--- src/components/personal-form/index.tsx
+++ src/components/personal-form/index.tsx
@@ -2,7 +2,16 @@ import Input from "../../UI/input";
 import Button from "../../UI/button";
 import { useFormik } from "formik";
 import { PersonalType } from "../../models";
-import { initialValues, validate } from "../utils";
+import { initialValues } from "../utils";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+  firstName: Yup.string().required("firstName is required !"),
+  lastName: Yup.string().required("lastName is Required !"),
+  email: Yup.string()
+    .email("Is Not Email Format Validate")
+    .required("Email is Required !"),
+});
 
 const PersonalForm = () => {
   const formik = useFormik<PersonalType>({
@@ -11,21 +20,20 @@ const PersonalForm = () => {
       console.log(data);
       formik.resetForm();
     },
-    validate,
+    validationSchema,
   });
   const className = "border w-full  rounded-lg p-1";
 
-  console.log("Formik Toched", formik.touched);
   return (
     <div className="flex justify-center items-center w-full p-5">
-      <form className="rounded-lg shadow-md flex flex-col justify-center items-start w-1/2 border p-2 gap-6">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="rounded-lg shadow-md flex flex-col justify-center items-start w-1/2 border p-2 gap-6"
+      >
         <Input
           lable="firstName"
-          name="firstName"
-          onChange={formik.handleChange}
           className={className}
-          value={formik.values.firstName}
-          onBlur={formik.handleBlur}
+          {...formik.getFieldProps("firstName")}
           errorMessage={
             formik.touched.firstName && formik.errors.firstName
               ? formik.errors.firstName
@@ -34,36 +42,26 @@ const PersonalForm = () => {
         />
         <Input
           lable="lastName"
-          name="lastName"
-          onChange={formik.handleChange}
           className={className}
-          value={formik.values.lastName}
           errorMessage={
             formik.touched.lastName && formik.errors.lastName
               ? formik.errors.lastName
               : ""
           }
-          onBlur={formik.handleBlur}
+          {...formik.getFieldProps("lastName")}
         />
         <Input
           lable="email"
-          name="email"
-          onChange={formik.handleChange}
+          {...formik.getFieldProps("email")}
           className={className}
-          value={formik.values.email}
           errorMessage={
             formik.touched.email && formik.errors.email
               ? formik.errors.email
               : ""
           }
-          onBlur={formik.handleBlur}
         />
         <div className="flex w-full justify-center items-center">
           <Button
-            onClick={(e) => {
-              e.preventDefault();
-              formik.handleSubmit();
-            }}
             text="submite"
             className="bg-blue-700 w-1/4 p-1 rounded-lg text-white"
           />
